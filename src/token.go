29ff27d2e1283e6818dccc206957493bd7ea157f
Zachary Thompson <zachthompson406@gmail.com>
2025-05-31 21:54:50 -0700

    token type names can be printed

--- src/token.go
+++ src/token.go
@@ -50,17 +50,102 @@ const (
 	WHILE
 )
 
+func (t TokenType) String() string {
+	switch t {
+	case EOF:
+		return "EOF"
+	case LEFT_PAREN:
+		return "LEFT_PAREN"
+	case RIGHT_PAREN:
+		return "RIGHT_PAREN"
+	case LEFT_BRACE:
+		return "LEFT_BRACE"
+	case RIGHT_BRACE:
+		return "RIGHT_BRACE"
+	case COMMA:
+		return "COMMA"
+	case DOT:
+		return "DOT"
+	case MINUS:
+		return "MINUS"
+	case PLUS:
+		return "PLUS"
+	case SEMICOLON:
+		return "SEMICOLON"
+	case SLASH:
+		return "SLASH"
+	case STAR:
+		return "STAR"
+	case BANG:
+		return "BANG"
+	case BANG_EQUAL:
+		return "BANG_EQUAL"
+	case EQUAL:
+		return "EQUAL"
+	case EQUAL_EQUAL:
+		return "EQUAL_EQUAL"
+	case GREATER:
+		return "GREATER"
+	case GREATER_EQUAL:
+		return "GREATER_EQUAL"
+	case LESS:
+		return "LESS"
+	case LESS_EQUAL:
+		return "LESS_EQUAL"
+	case IDENTIFIER:
+		return "IDENTIFIER"
+	case STRING:
+		return "STRING"
+	case NUMBER:
+		return "NUMBER"
+	case AND:
+		return "AND"
+	case CLASS:
+		return "CLASS"
+	case ELSE:
+		return "ELSE"
+	case FALSE:
+		return "FALSE"
+	case FUN:
+		return "FUN"
+	case FOR:
+		return "FOR"
+	case IF:
+		return "IF"
+	case NIL:
+		return "NIL"
+	case OR:
+		return "OR"
+	case PRINT:
+		return "PRINT"
+	case RETURN:
+		return "RETURN"
+	case SUPER:
+		return "SUPER"
+	case THIS:
+		return "THIS"
+	case TRUE:
+		return "TRUE"
+	case VAR:
+		return "VAR"
+	case WHILE:
+		return "WHILE"
+	default:
+		return fmt.Sprintf("%d", int(t))
+	}
+}
+
 type Token struct {
-	tokenType TokenType
-	lexeme    string
-	literal   any
-	line      int
+	TokenType TokenType
+	Lexeme    string
+	Literal   any
+	Line      int
 }
 
 func NewToken(tokenType TokenType, lexeme string, literal any, line int) Token {
-	return Token{tokenType: tokenType, lexeme: lexeme, literal: literal, line: line}
+	return Token{TokenType: tokenType, Lexeme: lexeme, Literal: literal, Line: line}
 }
 
 func (t Token) toString() string {
-	return fmt.Sprintf("%d %s %v", t.line, t.lexeme, t.literal)
+	return fmt.Sprintf("%d %s %v", t.Line, t.Lexeme, t.Literal)
 }
